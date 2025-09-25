2ff0cb1abd19edbe7de28071eb4b23afbd9f9366
BilalAlAlawneh <bilalalalawneh@gmail.com>
2025-09-25 13:18:03 -0700

    gross pay calculator

---
+++ workbook1d/PayrollCalculator/src/main/java/com/pluralsight/PayRoll.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class PayRoll {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter your name: ");
+        String EmployeeName = scanner.nextLine();
+
+        System.out.printf("Enter your wage per hour: ");
+        float hrRate = scanner.nextFloat();
+
+        System.out.printf("Enter your hours worked: ");
+        float hrWorked = scanner.nextFloat();
+
+        System.out.printf(EmployeeName + " gross pay is: " + (hrRate*hrWorked));
+
+    }
+}
