ed36553f082a4144f601401a12cc06911d78dd62
BilalAlAlawneh <bilalalalawneh@gmail.com>
2025-10-01 11:48:52 -0700

    Cell Phone Service exercise updated with overloaded methods

--- workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -9,6 +9,10 @@ public class CellPhone {
     private String phoneNumber;
     private String owner;
 
+    public CellPhone(){
+
+
+    }
     public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
         this.serialNumber = serialNumber;
         this.model = model;
@@ -41,4 +45,23 @@ public class CellPhone {
         System.out.println(owner + "'s phone is calling " + phoneNumber);
     }
 
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -7,8 +7,17 @@ public class CellPhoneApplication {
         CellPhone phone = new CellPhone(9093933, "Iphone 15", "AT&T", "415-917-8022", "Bilal Al Alawneh");
         CellPhone phone2 = new CellPhone(8080808, "Samsung Galaxy S23", "Verizon", "212-555-9090", "Alex Smith");
 
+        //newPhone
+        CellPhone phone3 = new CellPhone();
+        phone3.setSerialNumber(7070707);
+        phone3.setModel("Google Pixel 9");
+        phone3.setCarrier("T-Mobile");
+        phone3.setPhoneNumber("646-222-3333");
+        phone3.setOwner("Ahmad A");
+
         display(phone);
         display(phone2);
+        display(phone3);
 
         phone.dial(phone2.getPhoneNumber());
     }
